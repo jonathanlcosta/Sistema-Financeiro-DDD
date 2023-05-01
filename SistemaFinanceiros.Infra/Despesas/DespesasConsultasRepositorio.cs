3bce7cd3aa3c466ea418f5deaec0ed9f9a175b12
Jonathan Costa <nick.costa12@hotmail.com>
2023-05-01 15:08:27 -0300

    Refatorando dapper

--- SistemaFinanceiros.Infra/Despesas/DespesasConsultasRepositorio.cs
+++ SistemaFinanceiros.Infra/Despesas/DespesasConsultasRepositorio.cs
@@ -23,34 +23,34 @@ namespace SistemaFinanceiros.Infra.Despesas
           var parametros = new DynamicParameters();
           parametros.Add("@Email", email);
 
-                    var query = @"SELECT 
-                COUNT(1) as QuantidadeDespesas,
-                SF.id as IdSistemaFinanceiro,
-                SF.nome as NomeSistema,
-                C.id as IdCategoria,
-                C.nome as NomeCategoria,
-                D.id as IdDespesa,
-                D.nome as NomeDespesa,
-                U.id as IdUsuario,
-                U.nome as NomeUsuario
-            FROM DESPESAS D
-            INNER JOIN USUARIOS U ON D.idUsuario = U.id
-            INNER JOIN CATEGORIAS C ON D.IdCategoria = C.id
-            INNER JOIN SISTEMAFINANCEIROS SF ON C.idSistemaFinanceiro = SF.id
-            WHERE U.email = @Email
-                AND D.pago = false
-                AND MONTH(D.dataVencimento) = MONTH(DATE_ADD(NOW(), INTERVAL -1 MONTH))
-                AND YEAR(D.dataVencimento) = YEAR(DATE_ADD(NOW(), INTERVAL -1 MONTH))
-            GROUP BY 
-                IdSistemaFinanceiro,
-                NomeSistema,
-                IdCategoria,
-                NomeCategoria,
-                IdDespesa,
-                NomeDespesa,
-                IdUsuario,
-                NomeUsuario
-            ORDER BY QuantidadeDespesas DESC";
+                    var query = @"SELECT
+                    COUNT(1) as QuantidadeDespesas,
+                    SF.id as IdSistemaFinanceiro,
+                    SF.nome as NomeSistema,
+                    C.id as IdCategoria,
+                    C.nome as NomeCategoria,
+                    D.id as IdDespesa,
+                    D.nome as NomeDespesa,
+                    U.id as IdUsuario,
+                    U.nome as NomeUsuario
+                    FROM DESPESAS D,
+                    USUARIOS U,
+                    CATEGORIAS C,
+                    SISTEMAFINANCEIROS SF
+                    WHERE D.idUsuario = U.id
+                    AND D.idCategoria = C.id
+                    AND C.idSistemaFinanceiro = SF.id
+                    AND U.email = @Email
+                    GROUP BY
+                    IdSistemaFinanceiro,
+                    NomeSistema,
+                    IdCategoria,
+                    NomeCategoria,
+                    IdDespesa,
+                    NomeDespesa,
+                    IdUsuario,
+                    NomeUsuario
+                    ORDER BY COUNT(1) DESC";
 
           return Listar(query, parametros, pagina, quantidade);
       }
