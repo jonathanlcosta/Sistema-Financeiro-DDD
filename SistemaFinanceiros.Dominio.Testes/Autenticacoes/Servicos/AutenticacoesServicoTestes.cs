2f4777ef66b4159f7005d50a1712bd72fec59935
Jonathan Costa <nick.costa12@hotmail.com>
2023-06-14 09:39:44 -0300

    Adicionando teste de autenticacao servico

--- SistemaFinanceiros.Dominio.Testes/Autenticacoes/Servicos/AutenticacoesServicoTestes.cs
+++ SistemaFinanceiros.Dominio.Testes/Autenticacoes/Servicos/AutenticacoesServicoTestes.cs
@@ -19,11 +19,11 @@ namespace SistemaFinanceiros.Dominio.Testes.Autenticacoes.Servicos
 
         public AutenticacoesServicoTestes()
         {
-             usuarioValido = Builder<Usuario>.CreateNew().Build();
-             sut = Substitute.For<AutenticacoesServico>();
+            usuarioValido = Builder<Usuario>.CreateNew().Build();
+            sut = Substitute.For<AutenticacoesServico>();
         }
 
-        public class GerarToken: AutenticacoesServicoTestes
+        public class GerarToken : AutenticacoesServicoTestes
         {
             [Fact]
             public void Quando_ParametrosForemValidos_Espero_TokenGerado()
@@ -34,15 +34,50 @@ namespace SistemaFinanceiros.Dominio.Testes.Autenticacoes.Servicos
             }
         }
 
-        public class ValidarCadastro: AutenticacoesServicoTestes
+        public class ValidarCadastro : AutenticacoesServicoTestes
         {
-           [Fact]
+            [Fact]
             public void Quando_EmailESenhaForVazio_Espero_RegraDeNegocioExcecao()
             {
-               string email = "";
-               string senha = "";
+                string email = "";
+                string senha = "";
                 sut.Invoking(x => x.ValidarCadastro(email, senha)).Should().Throw<RegraDeNegocioExcecao>();
             }
+
+            [Fact]
+            public void Dado_EmailESenhaValidos_Espero_PropriedadePreenchida()
+            {
+                string email = "leonardo@gmail.com";
+                string senha = "Aleatorio@123";
+                var resultado = sut.ValidarCadastro(email, senha);
+                resultado.Email.Should().Be(email);
+                resultado.Senha.Should().Be(senha);
+            }
+        }
+
+        public class ValidarLogin : AutenticacoesServicoTestes
+        {
+            [Fact]
+            public void Quando_UsuarioVirNulo_Espero_RegraDeNegocioExcecao()
+            {
+                string senha = "Aleatorio@123";
+                sut.Invoking(x => x.ValidarLogin(null, senha)).Should().Throw<RegraDeNegocioExcecao>();
+            }
+
+            [Fact]
+            public void Dado_UsuarioESenhaValidos_Espera_RetornarUsuario()
+            {
+
+                var senha = "Aleatorio@123";
+                var usuario = Builder<Usuario>.CreateNew().Build();
+                usuario.SetSenhaHash(BCrypt.Net.BCrypt.HashPassword(senha));
+
+                var resultado = sut.ValidarLogin(usuario, senha);
+
+                resultado.Should().NotBeNull();
+                resultado.Should().Be(usuario);
+            }
+
         }
     }
 }
